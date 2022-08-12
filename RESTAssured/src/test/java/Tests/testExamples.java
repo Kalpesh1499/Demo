9c2ed9694d0026475e15caafc299083fc9e6d820
kalpesh <patilkd146@gmail.com>
2022-08-12 15:40:05 +0530

    first commit

---
+++ RESTAssured/src/test/java/Tests/testExamples.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+public class testExamples {
+	@Test
+	public void test_1() {
+
+		Response rp = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(rp.getStatusCode());
+		System.out.println(rp.statusLine().toString());
+		System.out.println(rp.asPrettyString());
+	}
+
+	@Test
+	public void test_2() {
+		given().get("https://reqres.in/api/users?page=2");
+	}
+}
